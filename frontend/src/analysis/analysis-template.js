3730b26055340e0152ab801aadbea047f2a39979
Weldon Scott <wascott@uwaterloo.ca>
2023-07-18 23:31:14 -0400

    Merged metric choice component into new page template

--- frontend/src/analysis/analysis-template.js
+++ frontend/src/analysis/analysis-template.js
@@ -1,17 +1,16 @@
 import React, { useContext, useEffect, useState } from 'react';
-import { Box, Button, Container, Skeleton, ToggleButton, ToggleButtonGroup, Typography } from '@mui/material';
+import { Box, Button, Checkbox, Container, FormControl, Grid, InputLabel, MenuItem, Select, Skeleton, ToggleButton, ToggleButtonGroup, Typography } from '@mui/material';
 import { DataGrid } from '@mui/x-data-grid';
 import { Form, useNavigate } from 'react-router-dom';
 import { Bar, BarChart, CartesianGrid, Tooltip, XAxis, YAxis } from "recharts";
 
-import { CountryContext } from "../app"
+import Client from '../api/client';
+import { CountryContext } from "../app";
 import CountryFlagCell from '../components/country-flag-cell';
-import MetricSelectionPanel from '../components/metric-choices';
 
 //TODO make this ts
 const AnalysisTemplate = ({ title, dataKey, metrics, columns }) => {
     const [data, setData] = useState(undefined);
-    const [hasSelected, setHasSelected] = useState(false);
 
     const [table, setTable] = useState(true);
 
@@ -19,11 +18,33 @@ const AnalysisTemplate = ({ title, dataKey, metrics, columns }) => {
 
     const navigate = useNavigate();
 
+    const [metric, setMetric] = useState(metrics[0])
+    const [choices, setChoices] = useState(metric.getDefaultValueObject())
+    const [updateCount, setUpdateCount] = useState(0);
+
     useEffect(() => {
-        if (data) {
-            setHasSelected(true);
+        if (countries) {
+            Client.post(metric.url, {
+                ...choices
+            })
+                .then(res => {
+                    const newData = res.data.map((elem, index) => {
+                        return {
+                            ...elem,
+                            country: countries ? countries.find(country => country.id === elem.country) : undefined,
+                            id: index
+                        }
+                    })
+
+                    setData(newData);
+                });
         }
-    }, [data])
+    }, [updateCount]);
+
+    const handleUpdate = () => {
+        setUpdateCount(n => n + 1);
+        setData(undefined);
+    }
 
     return (
         <Container sx={{
@@ -33,19 +54,102 @@ const AnalysisTemplate = ({ title, dataKey, metrics, columns }) => {
         }}>
             <Typography variant="h3" align="center">{title}</Typography>
 
-            <MetricSelectionPanel
-                metrics={metrics}
-                setData={setData}
-                processData={(data) =>
-                    data.map((elem, index) => {
-                        return {
-                            ...elem,
-                            country: countries ? countries.find(country => country.id === elem.country) : undefined,
-                            id: index
+            <Grid
+                container
+                borderRadius="10px"
+                width="50%"
+                p={1}
+                m={1}
+                justifyContent="center"
+                sx={{
+                    backgroundColor: "#eee",
+                }}>
+                <Grid item xs={12}>
+                    <Typography variant="h6" align="center" mb={1}>Settings</Typography>
+                </Grid>
+
+                <Grid item xs={12} display="flex" justifyContent="center" mb={2}>
+                    <FormControl>
+                        <InputLabel id="metric-label">Metric</InputLabel>
+                        <Select
+                            labelId="metric-label"
+                            label="Metric"
+                            value={metric}
+                            onChange={(e) => {
+                                setChoices(e.target.value.getDefaultValueObject());
+                                setMetric(e.target.value);
+                            }}
+                        >
+                            {metrics.map(metric => (
+                                <MenuItem key={metric.url} value={metric}>{metric.label}</MenuItem>
+                            ))}
+                        </Select>
+                    </FormControl>
+                </Grid>
+
+                {metric.parameters.map(param =>
+                    <Grid
+                        key={param.name}
+                        item
+                        xs={6}
+                        display="flex"
+                        flexDirection="column"
+                        alignItems="center"
+                        justifyContent="center"
+                        mt={1}
+                    >
+                        {param.type === "boolean" ?
+                            <>
+                                <InputLabel id={`${param.name}-label`}>{param.label}</InputLabel>
+                                <Checkbox
+                                    checked={choices[param.name]}
+                                    onChange={(e) => {
+                                        setChoices(
+                                            currentChoices => {
+                                                let newChoices = { ...currentChoices };
+                                                newChoices[param.name] = e.target.checked;
+                                                return newChoices;
+                                            }
+                                        )
+                                    }}
+                                />
+                            </>
+
+                            :
+                            <FormControl sx={{ width: "70%" }}>
+                                <InputLabel id={`${param.name}-label`}>{param.label}</InputLabel>
+                                <Select
+                                    labelId={`${param.name}-label`}
+                                    label={param.label}
+                                    value={choices[param.name]}
+                                    onChange={(e) => {
+                                        setChoices(
+                                            currentChoices => {
+                                                let newChoices = { ...currentChoices };
+                                                newChoices[param.name] = e.target.value;
+                                                return newChoices;
+                                            }
+                                        )
+                                    }}>
+                                    {
+                                        param.choices.map(choice => (
+                                            <MenuItem key={choice.value} value={choice.value}>{choice.label}</MenuItem>
+                                        ))
+                                    }
+                                </Select>
+                            </FormControl>
+
                         }
-                    })
-                }
-            />
+
+                    </Grid>
+
+
+                )}
+
+                <Grid item xs={12} display="flex" justifyContent="end">
+                    <Button onClick={handleUpdate}>Update</Button>
+                </Grid>
+            </Grid>
 
 
             <Box sx={{
@@ -68,7 +172,7 @@ const AnalysisTemplate = ({ title, dataKey, metrics, columns }) => {
             </Box>
 
             {
-                hasSelected ?
+                updateCount > 0 ?
                     (
                         data ?
                             (
