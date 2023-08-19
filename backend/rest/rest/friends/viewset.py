a0f7b5c0d53b33eea6b1784b8430bf3a298e07ba
Weldon Scott <wascott@uwaterloo.ca>
2023-08-18 20:17:43 -0400

    Added Spearman rank correlation

--- backend/rest/rest/friends/viewset.py
+++ backend/rest/rest/friends/viewset.py
@@ -1,12 +1,20 @@
+from enum import Enum
 import numpy as np
 from rest_framework import viewsets
+from scipy.stats import spearmanr
 
 from rest.countries.viewset import CountrySerializer
 from models import Country, Show, ShowType, Vote, VoteType
 
 
 class FriendViewSet(viewsets.GenericViewSet):
-    def get_ranking_matrix(self, year: int, vote_type: int):
+    class RankType(Enum):
+        RAW_RANK = 0
+        PROPORTION = 1
+
+    def get_ranking_matrix(
+        self, year: int, vote_type: VoteType, rank_type: RankType = RankType.PROPORTION
+    ):
         """
         Returns a "matrix" containing the points given to each entry by each voting country.
         The actual data structure will be a dict of dicts.
@@ -40,12 +48,15 @@ class FriendViewSet(viewsets.GenericViewSet):
 
             # Let's iterate through the vote's rankings
             for i in range(len(vote.ranking)):
-                value = 1 - i / (len(vote.ranking) - 1)
-                matrix[code][vote.ranking[i]] = value
+                if rank_type == self.RankType.RAW_RANK:
+                    matrix[code][vote.ranking[i]] = i + 1
+                elif rank_type == self.RankType.PROPORTION:
+                    value = 1 - i / (len(vote.ranking) - 1)
+                    matrix[code][vote.ranking[i]] = value
 
         return matrix
 
-    def get_jury_televote_similarity(self, year: int):
+    def get_jury_televote_cosine_similarity(self, year: int):
         """
         Gets a quantification of the difference between the jury and televote results for each country
         We'll use cosine similarity for this
@@ -94,7 +105,51 @@ class FriendViewSet(viewsets.GenericViewSet):
 
         return lst
 
-    def get_similarity_matrix(self, year: int):
+    def get_jury_televote_rank_similarity(self, year: int):
+        """
+        Gets a quantification of the difference between the jury and televote results for each country
+        This uses the Spearman rank correlation coefficient
+        """
+
+        result_dict = {}
+
+        # Get our matrices
+        jury_matrix = self.get_ranking_matrix(year, VoteType.JURY)
+        televote_matrix = self.get_ranking_matrix(year, VoteType.TELEVOTE)
+
+        for voter in jury_matrix:
+            if voter not in televote_matrix:
+                continue
+
+            jury = []
+            televote = []
+
+            # get entries in the same order
+            for receiver in jury_matrix[voter]:
+                if receiver not in televote_matrix[voter]:
+                    continue
+
+                jury.append(jury_matrix[voter][receiver])
+                televote.append(televote_matrix[voter][receiver])
+
+            # now let's calculate the rank correlation
+            correlation = spearmanr(jury, televote).statistic
+
+            result_dict[voter] = correlation
+
+        lst = [
+            {
+                "country": CountrySerializer(Country.objects.get(code=k)).data,
+                "similarity": v,
+            }
+            for k, v in result_dict.items()
+        ]
+
+        lst = sorted(lst, key=lambda x: x["similarity"], reverse=True)
+
+        return lst
+
+    def get_cosine_similarity_matrix(self, year: int):
         """
         Gets a "matrix" of the similarity between each pair of countries' voting.
         We'll use cosine similarity to quantify this.
@@ -152,13 +207,79 @@ class FriendViewSet(viewsets.GenericViewSet):
 
         return matrix
 
-    def get_ranked_similarities(self, year: int):
+    def get_rank_similarity_matrix(self, year: int):
+        # Get our matrices
+        jury_matrix = self.get_ranking_matrix(year, VoteType.JURY)
+        televote_matrix = self.get_ranking_matrix(year, VoteType.TELEVOTE)
+
+        # Sum them
+        sum = {
+            voter: {
+                votee: jury_matrix[voter][votee] + televote_matrix[voter][votee]
+                for votee in jury_matrix[voter]
+            }
+            for voter in jury_matrix
+        }
+
+        matrix = {}
+
+        # For each pair of countries, calculate the similarity
+        for country_a in sum:
+            for country_b in sum:
+                # Let's skip this scenario to speed things up
+                if country_a >= country_b:
+                    continue
+
+                # Get vectors of scores (making sure to have them in the same order!)
+                a_scores = [
+                    sum[country_a][country]
+                    for country in sum[country_a]
+                    if country != country_b
+                ]
+
+                b_scores = [
+                    sum[country_b][country]
+                    for country in sum[country_a]
+                    if country != country_b
+                ]
+
+                # Calculate similarity
+                correlation = spearmanr(a_scores, b_scores).statistic
+
+                if country_a not in matrix:
+                    matrix[country_a] = {}
+
+                matrix[country_a][country_b] = correlation
+
+        return matrix
+
+    def get_ranked_cosine_similarities(self, year: int):
         """
         Get all country pairs ranked by similarity in voting
         """
 
         # Get the similarity matrix
-        matrix = self.get_similarity_matrix(year)
+        matrix = self.get_cosine_similarity_matrix(year)
+
+        lst = [
+            {
+                "countries": [
+                    CountrySerializer(Country.objects.get(code=country_a)).data,
+                    CountrySerializer(Country.objects.get(code=country_b)).data,
+                ],
+                "similarity": matrix[country_a][country_b],
+            }
+            for country_a in matrix
+            for country_b in matrix[country_a]
+        ]
+
+        lst = sorted(lst, key=lambda x: x["similarity"], reverse=True)
+
+        return lst
+
+    def get_ranked_rank_similarities(self, year: int):
+        # Get the similarity matrix
+        matrix = self.get_rank_similarity_matrix(year)
 
         lst = [
             {
